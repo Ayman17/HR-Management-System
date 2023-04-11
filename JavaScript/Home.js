6d9dc2ee26ec516813c431f394c6886ab199e31b
amansour13 <a.mansour.dev1@gmail.com>
2023-04-11 02:28:14 +0200

    update some styling

--- JavaScript/Home.js
+++ JavaScript/Home.js
@@ -1,3 +1,10 @@
 function OurProject() {
     window.open("HrHome.html");
 }
+let el = document.getElementById("animate");
+el.addEventListener("mouseover", ()=>{
+    el.style.width = "40vw";
+});
+el.addEventListener("mouseout", ()=>{
+    el.style.width = "20vw";
+});
