d986a7d444fbd9edf25b73f4579f276e559bb9b4
AhmedOsama9 <ahmedosamaa975@gmail.com>
2023-04-13 01:20:37 +0200

    Added the hamburger

--- JavaScript/Home.js
+++ JavaScript/Home.js
@@ -1,10 +1,56 @@
-function OurProject() {
-    window.open("HrHome.html");
-}
-let el = document.getElementById("animate");
-el.addEventListener("mouseover", ()=>{
+//Constants
+const el = document.getElementById("animate");
+
+//The hamburger
+const toggle = document.querySelector('.navbar_toggle');
+const nav = document.querySelector('.navigation');
+const content = document.querySelector('.content');
+
+//using the media query to remove animate
+const smallScreen = window.matchMedia("(max-width: 800px)");
+const homeHeader = document.getElementById("home_header");
+
+
+//Functions
+function mouseOver(){
     el.style.width = "40vw";
-});
-el.addEventListener("mouseout", ()=>{
+}
+
+function mouseOut(){
     el.style.width = "20vw";
-});
+}
+
+function hamburgerClicked(){
+    toggle.classList.toggle('active');
+    if (nav) nav.classList.toggle('active');
+    if (content) content.classList.toggle('active');
+    if (homeHeader) homeHeader.classList.toggle('active');
+}
+
+function addAnimate(){
+    //the normal is to add them
+    el.addEventListener("mouseover", mouseOver);
+    el.addEventListener("mouseout", mouseOut);
+}
+
+function checkSmallScreen(){
+    if(smallScreen.matches){
+        el.removeEventListener("mouseover", mouseOver);
+        el.removeEventListener("mouseout", mouseOut);
+    }
+}
+
+
+//The Main function 
+
+function Main() {
+    window.open("HrHome.html");
+
+    toggle.addEventListener('click', hamburgerClicked);
+    
+    addAnimate();
+    checkSmallScreen();
+}
+
+
+Main();
