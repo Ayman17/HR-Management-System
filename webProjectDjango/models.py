d7166c42309fe22f1ffe8703a9b2d35fd3eeb82d
AhmedOsama9 <ahmedosamaa975@gmail.com>
2023-05-23 02:18:14 +0300

    Employee Table

---
+++ webProjectDjango/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+
+class Employee(models.Model):
+    
+    id = models.IntegerField(primary_key=True)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
+    address = models.CharField(max_length=100)
+    phone = models.CharField(max_length=20)
+    GENDER_CHOICES = (
+        ('male', 'Male'),
+        ('female', 'Female'),
+    )
+    gender = models.CharField(max_length=10, choices=GENDER_CHOICES, default='male')
+    MARITAL_STATUS_CHOICES = (
+        ('married', 'Married'),
+        ('unmarried', 'Unmarried'),
+    )
+    marital_status = models.CharField(max_length=10, choices=MARITAL_STATUS_CHOICES, default='married')
+    vacation_available = models.IntegerField()
+    vacation_actual = models.IntegerField()
+    salary = models.DecimalField(max_digits=10, decimal_places=2)
+    birth_date = models.DateField()
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
--- webProjectDjango/views.py
+++ webProjectDjango/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+import re
+from .models import Employee
 
 def index(request):
     return render(request, 'Index.html')
@@ -20,3 +23,41 @@ def vacation(request):
 
 def submit_vacation(request):
     return render(request, 'submittedVacations.html')
+
+def add_new(request):
+    if request.method == 'POST':
+        # Retrieve form data from the request
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        email = request.POST.get('email')
+        address = request.POST.get('address')
+        id = request.POST.get('id')
+        phone = request.POST.get('phone')
+        gender = request.POST.get('gender')
+        marital_status = request.POST.get('marital_status')
+        vacation_available = request.POST.get('vacation_available')
+        vacation_actual = request.POST.get('vacation_actual')
+        salary = request.POST.get('salary')
+        birth_date = request.POST.get('birth_date')
+
+        employee = Employee(
+            first_name=first_name,
+            last_name=last_name,
+            email=email,
+            address=address,
+            id=id,
+            phone=phone,
+            gender=gender,
+            marital_status=marital_status,
+            vacation_available=vacation_available,
+            vacation_actual=vacation_actual,
+            salary=salary,
+            birth_date=birth_date
+        )
+
+        employee.save()
+
+        return redirect('hr_home')
+
+    return render(request, 'addNew.html')
+
