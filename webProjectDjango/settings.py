8935902483a9827c1c31361a3bde809777246b5c
amansour13 <a.mansour.dev1@gmail.com>
2023-05-24 18:11:49 +0300

    adjust styles and adding .gitignore file

--- webProjectDjango/settings.py
+++ webProjectDjango/settings.py
@@ -128,3 +128,12 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# to disable django taking cashe
+# this line added for easily development
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.dummy.DummyCache',
+    }
+}
